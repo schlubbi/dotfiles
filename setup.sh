edc06a325bdaf8d930c9d46e11f2622619e10fca
Stefan Jöst <schlubbi@github.com>
2022-07-01 09:25:02 +0200

    foobar

--- setup.sh
+++ setup.sh
@@ -1,2 +1,13 @@
+function install_neovim {
+  curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+  chmod u+x nvim.appimage
+  ./nvim.appimage --appimage-extract
+  ./squashfh-root/AppRun --version
+  sudo mv squashfs-root /
+  sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+}
+install_neovim
+
 mkdir ~/.config/
 ln -s /workspaces/.codespaces/.persistedshare/dotfiles/nvim ~/.config/nvim
+ln -s /workspaces/.codespaces/.persistedshare/dotfiles/.gitconfig ~/.gitconfig
