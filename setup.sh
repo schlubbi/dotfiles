f97131c4742b0d27a8e91990b848e31053c9547b
Stefan Jöst <schlubbi@github.com>
2022-12-23 09:35:47 +0100

    simplify nvim installg

--- setup.sh
+++ setup.sh
@@ -1,13 +1,11 @@
+#!/usr/bin/env bash 
+
 function install_neovim {
-  curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-  chmod u+x nvim.appimage
-  ./nvim.appimage --appimage-extract
-  ./squashfh-root/AppRun --version
-  sudo mv squashfs-root /
-  sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+  curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.deb
+  sudo dpkg -i nvim-linux64.deb
 }
 install_neovim
 
 mkdir ~/.config/
-ln -s /workspaces/.codespaces/.persistedshare/dotfiles/nvim ~/.config/nvim
+ln -sf /workspaces/.codespaces/.persistedshare/dotfiles/nvim ~/.config/nvim
 ln -sf /workspaces/.codespaces/.persistedshare/dotfiles/.gitconfig ~/.gitconfig
