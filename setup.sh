33111524a3dccbe614806418c9c43eb4147e97d7
Stefan Jöst <schlubbi@github.com>
2023-09-18 11:29:58 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,29 @@
 #!/usr/bin/env bash 
 
 function install_neovim {
-  curl -LO https://github.  curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
-  tar xzvf nvim-linux64.tar.gz
-  sudo cp -r nvim-linux64/ /usr/local/
-  rm -rf nvim-linux64.tar.gz nvim-linux64
+ curl -LO https://github.  curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
+ tar xzvf nvim-linux64.tar.gz
+ sudo cp -r nvim-linux64/ /usr/local/
+ rm -rf nvim-linux64.tar.gz nvim-linux64
 }
 
 function install_node {
-	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
-	source ~/.bashrc
-	nvm install node
+  if ! loc="$(type -p "node")" || [[ -z $loc ]]; then
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+    source ~/.bashrc
+    nvm install node
+  fi
 }
 
-mkdir ~/.config/
+function install_fzf {
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  ~/.fzf/install --all
+}
+
+mkdir -p ~/.config/
 
 install_node
+install_fzf
+install_neovim
 ln -sf /workspaces/.codespaces/.persistedshare/dotfiles/.gitconfig ~/.gitconfig
+ln -sf /workspaces/.codespaces/.persistedshare/dotfiles/nvim_new ~/.config/nvim
