306a59f63f4df901c3b1d41aa34e4cbcc1ea60cc
Stefan Jöst <schlubbi@github.com>
2023-09-18 12:31:39 +0200

    Update core.lua

--- nvim_new/lua/plugins/core.lua
+++ nvim_new/lua/plugins/core.lua
@@ -1,19 +1,19 @@
 return {
-	{ "shaunsingh/nord.nvim" },
-	{ "junegunn/fzf", dir = "~/.fzf", build = "./install --all" },
-	{ "junegunn/fzf.vim" },
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function () 
-			local configs = require("nvim-treesitter.configs")
-
-			configs.setup({
-				ensure_installed = { "c", "lua", "vim", "query", "elixir", "heex", "javascript", "html", "ruby" },
-				sync_install = false,
-				highlight = { enable = true },
-				indent = { enable = true },  
-			})
-		end
- },
+  { "shaunsingh/nord.nvim" },
+  { "junegunn/fzf", dir = "~/.fzf", build = "./install --all" },
+  { "junegunn/fzf.vim" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+      build = ":TSUpdate",
+      config = function () 
+	  local configs = require("nvim-treesitter.configs")
+          configs.setup({
+            ensure_installed = { "c", "lua", "vim", "query", "elixir", "heex", "javascript", "html", "ruby" },
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },  
+          })
+      end
+  },
+  { "github/copilot.vim" },
 }
