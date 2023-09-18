a487ff799eafca248f8b47fe7b337e2e065e4b49
Stefan Jöst <schlubbi@github.com>
2023-09-18 12:28:28 +0200

    Update init.lua

--- nvim_new/init.lua
+++ nvim_new/init.lua
@@ -20,8 +20,27 @@ end)
 
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+require('lspconfig').sorbet.setup{}
 
 lsp.setup()
+
+-- You need to setup `cmp` after lsp-zero
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+  mapping = {
+    -- `Enter` key to confirm completion
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+    -- Ctrl+Space to trigger completion menu
+    ['<C-Space>'] = cmp.mapping.complete(),
+
+    -- Navigate between snippet placeholder
+    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+  }
+})
 
 require("mason-lspconfig").setup {
     ensure_installed = {
